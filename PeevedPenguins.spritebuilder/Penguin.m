344094fec9f08cf1c661248d007f8edbc9a0e6a3
krisleba <christophe.leba@gmail.com>
2014-07-15 01:27:51 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,23 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by kris on 7/15/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    return self;
+}
+
+
+@end
