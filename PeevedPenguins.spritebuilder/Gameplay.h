31cb57206f2074000a472ca68208aa18eb0c452a
krisleba <christophe.leba@gmail.com>
2014-07-18 21:44:31 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,16 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by kris on 7/18/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode {
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+@end
